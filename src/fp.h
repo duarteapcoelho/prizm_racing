c82f15c3d03eea39427e6017a321ffc92f8af4a3
duarteapcoelho <duarte.ap.coelho@gmail.com>
2023-02-04 20:59:36 +0000

    Generalize fixed point class

--- src/fp.h
+++ src/fp.h
@@ -2,85 +2,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FP_RIGHT 1024
-#define FP_RIGHT_BITS 10
-class fp {
+template <typename T, int P, int M>
+class _fp {
 	public:
-	int i;
-	inline fp(int v){
-		i = v << FP_RIGHT_BITS;
+	T i;
+	template <typename T2, int P2, int M2>
+	inline _fp(_fp<T2, P2, M2> v){
+		int n = P - P2;
+		if(n > 0)
+			i = v.i << n;
+		else
+			i = v.i >> (-n);
 	}
-	inline fp(float v){
-		i = v * FP_RIGHT;
+	inline _fp(int v){
+		i = v << P;
 	}
-	inline fp(double v){
-		i = v * FP_RIGHT;
+	inline _fp(float v){
+		i = v * M;
 	}
-	inline fp(){ i = 0; }
+	inline _fp(double v){
+		i = v * M;
+	}
+	inline _fp(){ i = 0; }
 
-	inline operator int() { return i >> FP_RIGHT_BITS; }
-	inline operator float() { return ((float)i) / ((float)FP_RIGHT); }
+	inline operator int() { return i >> P; }
+	inline operator float() { return ((float)i) / ((float)M); }
 
-	inline fp operator + (fp o){
-		fp r;
+	inline _fp operator + (_fp<T,P,M> o){
+		_fp r;
 		r.i = i + o.i;
 		return r;
 	}
-	inline fp operator - (fp o){
-		fp r;
+	inline _fp operator - (_fp<T,P,M> o){
+		_fp r;
 		r.i = i - o.i;
 		return r;
 	}
-	inline fp operator * (fp o){
-		// printf("%d %d %d\n", i, o.i, INT_MAX);
-		fp r;
-		r.i = (i * o.i) >> FP_RIGHT_BITS;
+	inline _fp operator * (_fp<T,P,M> o){
+		_fp r;
+		r.i = (i * o.i) >> P;
 		return r;
 	}
-	inline fp operator / (fp o){
-		fp r;
-		r.i = (i<<FP_RIGHT_BITS) / o.i;
+	inline _fp operator / (_fp<T,P,M> o){
+		_fp r;
+		r.i = (i<<P) / o.i;
+		return r;
+	}
+	inline _fp operator % (_fp<T,P,M> o){
+		_fp r;
+		r.i = i % o.i;
 		return r;
 	}
 
-	// fp& operator += (const fp& o){
-	// 	i += o.i;
-	// 	return *this;
-	// }
-	// fp& operator -= (const fp& o){
-	// 	i += o.i;
-	// 	return *this;
-	// }
-	// fp& operator *= (const fp& o){
-	// 	i += o.i;
-	// 	return *this;
-	// }
-	// fp& operator /= (const fp& o){
-	// 	i += o.i;
-	// 	return *this;
-	// }
+	inline bool operator > (_fp<T,P,M> o){ return i > o.i; }
+	inline bool operator < (_fp<T,P,M> o){ return i < o.i; }
+	inline bool operator > (int o){ return i > (o<<P); }
+	inline bool operator < (int o){ return i < (o<<P); }
+	inline bool operator > (float o){ return i > o*M; }
+	inline bool operator < (float o){ return i < o*M; }
+	inline bool operator > (double o){ return i > o*M; }
+	inline bool operator < (double o){ return i < o*M; }
+	inline bool operator >= (_fp<T,P,M> o){ return i >= o.i; }
+	inline bool operator <= (_fp<T,P,M> o){ return i <= o.i; }
+	inline bool operator >= (int o){ return i >= (o<<P); }
+	inline bool operator <= (int o){ return i <= (o<<P); }
+	inline bool operator >= (float o){ return i >= o*M; }
+	inline bool operator <= (float o){ return i <= o*M; }
+	inline bool operator >= (double o){ return i >= o*M; }
+	inline bool operator <= (double o){ return i <= o*M; }
 
-	inline bool operator > (fp o){ return i > o.i; }
-	inline bool operator < (fp o){ return i < o.i; }
-	inline bool operator > (int o){ return i > (o<<FP_RIGHT_BITS); }
-	inline bool operator < (int o){ return i < (o<<FP_RIGHT_BITS); }
-	inline bool operator > (float o){ return i > o*FP_RIGHT; }
-	inline bool operator < (float o){ return i < o*FP_RIGHT; }
-	inline bool operator > (double o){ return i > o*FP_RIGHT; }
-	inline bool operator < (double o){ return i < o*FP_RIGHT; }
-	inline bool operator >= (fp o){ return i >= o.i; }
-	inline bool operator <= (fp o){ return i <= o.i; }
-	inline bool operator >= (int o){ return i >= (o<<FP_RIGHT_BITS); }
-	inline bool operator <= (int o){ return i <= (o<<FP_RIGHT_BITS); }
-	inline bool operator >= (float o){ return i >= o*FP_RIGHT; }
-	inline bool operator <= (float o){ return i <= o*FP_RIGHT; }
-	inline bool operator >= (double o){ return i >= o*FP_RIGHT; }
-	inline bool operator <= (double o){ return i <= o*FP_RIGHT; }
-
-	inline bool operator == (fp o){ return i == o.i; }
-	inline bool operator == (int o){ return i == (o<<FP_RIGHT_BITS); }
-	inline bool operator == (float o){ return i == o*FP_RIGHT; }
-	inline bool operator == (double o){ return i == o*FP_RIGHT; }
+	inline bool operator == (_fp<T,P,M> o){ return i == o.i; }
+	inline bool operator == (int o){ return i == (o<<P); }
+	inline bool operator == (float o){ return i == o*M; }
+	inline bool operator == (double o){ return i == o*M; }
+	inline bool operator != (_fp<T,P,M> o){ return i != o.i; }
+	inline bool operator != (int o){ return i != (o<<P); }
+	inline bool operator != (float o){ return i != o*M; }
+	inline bool operator != (double o){ return i != o*M; }
 };
 
-// typedef float fp;
+typedef _fp<int, 10, 1024> fp;
+typedef _fp<long long int, 10, 1024> lfp;
